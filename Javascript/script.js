9b3089b8ee5671dea019fefe641a8e482dbbdc26
SomaRe <shivatej82@gmail.com>
2021-04-16 12:36:16 +0530

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuOpen = document.querySelector('.menu-open');
+const mo = document.querySelectorAll('.mo');
+const equation = document.querySelector('.equation');
+let Open = false;
+menuBtn.addEventListener('click', () => {
+  if(!Open) {
+    Open = true;
+    menuBtn.classList.add('open');
+    menuOpen.classList.add('opened');
+    equation.style.display ="none";
+    let delay = 0;
+    mo.forEach(function (e){
+        e.classList.add('mo-slide');
+        e.style.transitionDelay = delay+"ms";
+        delay = delay+150;
+    });
+  } else {
+    Open = false;
+    menuBtn.classList.remove('open');
+    menuOpen.classList.remove('opened');
+    equation.style.display ="block";
+    mo.forEach(function (e){
+      e.classList.remove('mo-slide')
+  });
+  }
+});
