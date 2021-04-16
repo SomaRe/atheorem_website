3a83707ffbc1ecaf4c07d67bd87e118cd4d02888
SomaRe <shivatej82@gmail.com>
2021-04-16 13:49:02 +0530

    more corrected timing

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menuOpen = document.querySelector('.menu-open');
 const mo = document.querySelectorAll('.mo');
 const equation = document.querySelector('.equation');
+const row1 = document.querySelector('.row1');
 let Open = false;
 menuBtn.addEventListener('click', () => {
   if(!Open) {
@@ -9,6 +10,7 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('open');
     menuOpen.classList.add('opened');
     equation.style.display ="none";
+    row1.style.animationName = "";
     let delay = 0;
     mo.forEach(function (e){
         e.classList.add('mo-slide');
@@ -19,7 +21,8 @@ menuBtn.addEventListener('click', () => {
     Open = false;
     menuBtn.classList.remove('open');
     menuOpen.classList.remove('opened');
-    setTimeout(()=>equation.style.display ="block" ,800) ;
+    row1.style.animationName = "row1";
+    equation.style.display ="block" ;
     mo.forEach(function (e){
       e.classList.remove('mo-slide')
   });
